e263179497fa7ae2e39f6dc81670e73a94b78ad1
KevJSDev <KevJSDev@users.noreply.github.com>
2021-09-05 13:16:27 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from "react"
+const App = () => {
 
-function App() {
+  const [count, setcount] = useState(0)
+
+  const setUser = async () => {
+
+    const name = document.getElementById("name").value
+    debugger
+    const meta = {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: { username: `test-${count}-${name}`, displayname: `test-${count}`, password: `test-${count}`, imageurl: `test-${count}`}
+    }
+
+    const res = await fetch("https://localhost:5001/api/users", meta)
+    const data = await res.json()
+
+    // .then(data => {
+    console.log(data)
+    // })
+  }
+
+  useEffect(async () => {
+    const res = await fetch("https://localhost:5001/api/users")
+    const data = await res.json()
+    console.log(data)
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" id="name" name="name" size="10"/>
+      <button onClick={() => setUser()}>
+        set user
+      </button>
     </div>
   );
 }
